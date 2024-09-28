f7c54008d398985c69187d18a33270ec48d80a7d
shahinyahya <shahintyahya@gmail.com>
2024-09-28 18:42:17 +0530

    done basic crud operation usign sqlachemy

--- backend/blog/main.py
+++ backend/blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from schemas import Blog
 import schemas, models
 from database import engine, SessionLocal
@@ -20,7 +20,7 @@ def get_db():
 def home():
     return "This is my personal blog page"
 
-@app.post("/blog")
+@app.post("/blog", status_code=status.HTTP_201_CREATED)
 def create_blog(blog: Blog, db: Session = Depends(get_db)):   #! here db parameter is used to get db from it using depends
     new_blog = models.Blog(author = blog.author, title = blog.title, body = blog.body) #? creating new blog requesting each request data to model
     db.add(new_blog)
@@ -36,8 +36,45 @@ def get_all_blogs(db: Session = Depends(get_db)):
 @app.get("/blog/{id}")
 def single_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        #  response.status_code = status.HTTP_404_NOT_FOUND
+        #  return "Blog not found!"
+        raise HTTPException(status_code = 404, detail="The blog ain't found..")
     return blog
 
+
+"""  
+This is one way to delete a blog post. The other way is to delete using synchronize_session in sql alchemy which is show in next type
+"""
+
+# @app.delete("/blog/{id}")
+# def delete_blog(id: int, db: Session = Depends(get_db)):
+#     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    
+#     if not blog:
+#         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+#     db.delete(blog)
+#     db.commit()
+
+#     return "The blog has deleted successfully."
+
+#** Delete a blog
+
+@app.delete('/blog/{id}')
+def destroy(id: int, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    
+    return "The blog is deleted successfully"
+
+#** Update a blog 
+
+@app.put("/blog/{id}")
+def update_blog(id: int, blog: schemas.BlogUpdate, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).update({blog}, synchronize_session=False)
+    db.commit()
+    return "The blog has been updated"
 
 if __name__ == "__main__":
     import uvicorn
--- backend/blog/schemas.py
+++ backend/blog/schemas.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
 
+
+#? Creating a blog
 class Blog(BaseModel):
     author: str
+    title: str
+    body: str
+
+#** Update blog schema
+
+class BlogUpdate(BaseModel):
     title: str
     body: str
