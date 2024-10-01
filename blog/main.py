5568f8e7c878f96bf67406555088a481424d8509
shahinyahya <100275440+shahinyahya@users.noreply.github.com>
2024-10-01 18:01:13 +0530

    Added port and host in main.py

--- blog/main.py
+++ blog/main.py
@@ -22,4 +22,4 @@ app.include_router(user.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run("main:app", reload=True, host="0.0.0.0", port=80)
